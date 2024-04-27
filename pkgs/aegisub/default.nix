86d2824dd1233f2cdff000cbaaed4de3c1ba01f8
Hobr <mail@hobr.site>
2024-04-27 11:58:52 +0800

    new

--- pkgs/aegisub/default.nix
+++ pkgs/aegisub/default.nix
@@ -1,23 +1,67 @@
-{ lib, config, stdenv, fetchFromGitHub, fetchurl, fetchpatch,
+{
+  lib,
+  config,
+  stdenv,
+  fetchFromGitHub,
+  fetchurl,
+  fetchpatch,
 
-meson, cmake, luajit, ninja, pkg-config, wrapGAppsHook, wxGTK32, python3
-, gettext,
+  meson,
+  cmake,
+  luajit,
+  ninja,
+  pkg-config,
+  intltool,
+  python3,
+  gettext,
 
-boost, ffmpeg_4, ffms, fftw, fontconfig, icu, libGL, libGLU, libX11, libass
-, libiconv, libuchardet, zlib, jansson,
+  dav1d,
+  expat,
+  ffmpeg,
+  fftw,
+  freetype,
+  fontconfig,
+  fribidi,
+  harfbuzz,
+  icu,
+  jansson,
+  libass,
+  libGL,
+  libGLU,
+  libiconv,
+  libpng,
+  libuchardet,
+  libX11,
+  nasm,
+  wxGTK32,
+  zlib,
 
-alsaSupport ? stdenv.isLinux, alsa-lib, openalSupport ? true, openal
-, portaudioSupport ? false, portaudio
-, pulseaudioSupport ? config.pulseaudio or stdenv.isLinux, libpulseaudio
-, spellcheckSupport ? true, hunspell,
+  alsaSupport ? stdenv.isLinux,
+  alsa-lib,
+  openalSupport ? true,
+  openal,
+  portaudioSupport ? false,
+  portaudio,
+  pulseaudioSupport ? config.pulseaudio or stdenv.isLinux,
+  libpulseaudio,
+  spellcheckSupport ? true,
+  hunspell,
 
-darwin }:
+  darwin
+}:
 
 let
   inherit (darwin.apple_sdk.frameworks)
-    AppKit Carbon Cocoa CoreFoundation CoreText IOKit OpenAL QuartzCore;
+    AppKit
+    Carbon
+    Cocoa
+    CoreFoundation
+    CoreText
+    IOKit
+    OpenAL
+    QuartzCore;
 
-  luajit52 = luajit.override { enable52Compat = true; };
+  luajit = luajit.override { enable52Compat = true; };
 
   # from subprojects folder
   bestsource = fetchFromGitHub {
@@ -42,6 +86,18 @@ let
     fetchSubmodules = true;
   };
 
+  ffms2 = fetchFromGitHub {
+    owner = "arch1t3cht";
+    repo = "ffms2";
+    rev = "f463e4cae01e57f130742ebc7594a926da9d7261";
+    hash = "sha256-wt6FMQC57FMHHed+tJ2w+b/x/tswfmRM3WRBBKyufHg=";
+  };
+
+  boost = fetchurl {
+    url = "https://boostorg.jfrog.io/artifactory/main/release/1.74.0/source/boost_1_74_0.tar.gz";
+    hash = "sha256-r/8205KIUSC8rAeRSMF30fb3cw7D1HIzqlGwr6TblKU=";
+  };
+
   gtest = fetchurl {
     url = "https://github.com/google/googletest/archive/release-1.8.1.zip";
     hash = "sha256-kngnwYPQFzTMXP74Xg/z9akv/mGI4NGOkJxe/r8ooMc=";
@@ -51,7 +107,8 @@ let
     url = "https://wrapdb.mesonbuild.com/v1/projects/gtest/1.8.1/1/get_zip";
     hash = "sha256-959f1G4JUHs/LgmlHqbrIAIO/+VDM19a7lnzDMjRWAU=";
   };
-in stdenv.mkDerivation (finalAttrs: {
+in
+stdenv.mkDerivation (finalAttrs: {
   pname = "aegisub";
   version = "11";
 
@@ -65,45 +122,51 @@ in stdenv.mkDerivation (finalAttrs: {
   nativeBuildInputs = [
     meson
     cmake
-    luajit52
+    luajit
     ninja
     pkg-config
-    wrapGAppsHook
-    wxGTK32
+    intltool
     python3
     gettext
   ];
 
   buildInputs = [
-    boost
-    ffmpeg_4
-    ffms
+    dav1d
+    expat
+    ffmpeg
     fftw
+    freetype
     fontconfig
+    fribidi
+    harfbuzz
     icu
+    jansson
+    libass
     libGL
     libGLU
-    libX11
-    libass
     libiconv
+    libpng
     libuchardet
+    libX11
+    nasm
     wxGTK32
     zlib
-    jansson
-  ] ++ lib.optionals alsaSupport [ alsa-lib ] ++ lib.optionals openalSupport
-    [ (if stdenv.isDarwin then OpenAL else openal) ]
-    ++ lib.optionals portaudioSupport [ portaudio ]
-    ++ lib.optionals pulseaudioSupport [ libpulseaudio ]
-    ++ lib.optionals spellcheckSupport [ hunspell ]
-    ++ lib.optionals stdenv.isDarwin [
-      AppKit
-      Carbon
-      Cocoa
-      CoreFoundation
-      CoreText
-      IOKit
-      QuartzCore
-    ];
+  ]
+  ++ lib.optionals alsaSupport [ alsa-lib ]
+  ++ lib.optionals openalSupport [ openal ]
+  ++ lib.optionals portaudioSupport [ portaudio ]
+  ++ lib.optionals pulseaudioSupport [ libpulseaudio ]
+  ++ lib.optionals spellcheckSupport [ hunspell ]
+  ++ lib.optionals stdenv.isDarwin [
+    AppKit
+    Carbon
+    Cocoa
+    CoreFoundation
+    CoreText
+    IOKit
+    OpenAL
+    QuartzCore
+  ];
 
   patches = [
     # Replace bestaudiosource with bestscore
@@ -119,14 +182,14 @@ in stdenv.mkDerivation (finalAttrs: {
     ./0003-respack-unable-run.patch
   ];
 
-  env = {
-    BOOST_INCLUDEDIR = "${lib.getDev boost}/include";
-    BOOST_LIBRARYDIR = "${lib.getLib boost}/lib";
-  };
-
   mesonBuildType = "release";
+  dontUseCmakeConfigure = true;
 
   mesonFlags = [
+    "--force-fallback-for=ffms2"
+    (lib.mesonOption "default_library" "static")
+    (lib.mesonBool "local_boost" true)
+
     (lib.mesonOption "default_audio_output" "auto")
     (lib.mesonEnable "alsa" alsaSupport)
     (lib.mesonEnable "openal" openalSupport)
@@ -144,26 +207,24 @@ in stdenv.mkDerivation (finalAttrs: {
     (lib.mesonEnable "uchardet" true)
     (lib.mesonEnable "csri" true)
     (lib.mesonEnable "hunspell" spellcheckSupport)
-  ] ++ lib.optionals stdenv.isDarwin [
-    # Follow https://github.com/arch1t3cht/Aegisub/blob/feature_11/.github/workflows/ci.yml
-    "--force-fallback-for=ffms2"
-    (lib.mesonOption "default_library" "static")
-    (lib.mesonBool "build_osx_bundle" true)
-    (lib.mesonBool "local_boost" true)
-  ];
+  ]
+  ++ lib.optionals stdenv.isDarwin [ (lib.mesonBool "build_osx_bundle" true) ];
 
   preConfigure = ''
     cp -r --no-preserve=mode ${bestsource} subprojects/bestsource
     cp -r --no-preserve=mode ${AviSynthPlus} subprojects/avisynth
     cp -r --no-preserve=mode ${vapoursynth} subprojects/vapoursynth
+    cp -r --no-preserve=mode ${ffms2} subprojects/ffms2
 
     mkdir subprojects/packagecache
     cp -r --no-preserve=mode ${gtest} subprojects/packagecache/gtest-1.8.1.zip
     cp -r --no-preserve=mode ${gtest_patch} subprojects/packagecache/gtest-1.8.1-1-wrap.zip
+    cp -r --no-preserve=mode ${boost} subprojects/packagecache/boost_1_74_0.tar.gz
 
     meson subprojects packagefiles --apply bestsource
     meson subprojects packagefiles --apply avisynth
     meson subprojects packagefiles --apply vapoursynth
+    meson subprojects packagefiles --apply ffms2
   '';
 
   meta = {
@@ -177,7 +238,9 @@ in stdenv.mkDerivation (finalAttrs: {
     '';
     # The Aegisub sources are itself BSD/ISC, but they are linked against GPL'd
     # softwares - so the resulting program will be GPL
-    license = with lib.licenses; [ bsd3 ];
+    license = with lib.licenses; [
+      bsd3
+    ];
     mainProgram = "aegisub";
     platforms = lib.platforms.unix;
   };
