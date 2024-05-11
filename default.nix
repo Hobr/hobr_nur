172ae1ef91c611ad6a8e7acb09aded96bc22aa68
Hobr <mail@hobr.site>
2024-05-11 13:13:31 +0800

    oh-my-rime

--- default.nix
+++ default.nix
@@ -9,6 +9,7 @@
 
   aegisub = pkgs.callPackage ./pkgs/aegisub { };
   rime-ice = pkgs.callPackage ./pkgs/rime-ice { };
+  oh-my-rime = pkgs.callPackage ./pkgs/oh-my-rime { };
   # rife-ncnn-vulkan = pkgs.callPackage ./pkgs/rife-ncnn-vulkan { };
   # real-esrgan = pkgs.callPackage ./pkgs/real-esrgan { };
 }
