44f82f900517f9ac212f8c89ad6a3e1bbf10daeb
Hobr <mail@hobr.site>
2024-05-23 14:38:16 +0800

    format

--- pkgs/aegisub/default.nix
+++ pkgs/aegisub/default.nix
@@ -275,8 +275,10 @@ stdenv.mkDerivation (finalAttrs: {
         'ac_cv_prog_SETFILE="${setfile}/bin/SetFile"'
       substituteInPlace subprojects/wxWidgets/configure --replace \
         "-framework System" "-lSystem"
+
       sed -i '18i\#define wxUSE_THREADS 1' subprojects/wxWidgets/include/wx/thrimpl.cpp
       sed -i '17i\#include "thread.h"' subprojects/wxWidgets/include/wx/thrimpl.cpp
+
       meson subprojects packagefiles --apply wxWidgets
     '';
 
