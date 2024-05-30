e15d8638c3cb8148300decd01ee918d943514817
Hobr <mail@hobr.site>
2024-05-30 11:40:30 +0800

    uvm init

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
 
   aegisub = pkgs.callPackage ./pkgs/aegisub { };
   rime-ice = pkgs.callPackage ./pkgs/rime-ice { };
-  # ultimatevocalremovergui= pkgs.callPackage ./pkgs/ultimatevocalremovergui { };
+  ultimatevocalremovergui = pkgs.callPackage ./pkgs/ultimatevocalremovergui { };
   # atopile = pkgs.callPackage ./pkgs/atopile { };
   # storyboarder = pkgs.callPackage ./pkgs/storyboarder { };
   # paperlib = pkgs.callPackage ./pkgs/paperlib { };
