8266a1f53175334f601965ddc5fb4464d2781255
Hobr <mail@hobr.site>
2024-09-06 15:36:40 +0800

    uesless

--- default.nix
+++ default.nix
@@ -9,40 +9,4 @@
   aegisub = pkgs.callPackage ./pkgs/aegisub { };
 
   rime-ice = pkgs.callPackage ./pkgs/rime-ice { };
-
-  atopile = pkgs.callPackage ./pkgs/atopile {
-    case-converter = pkgs.callPackage ./pkgs/case-converter { };
-    eseries = pkgs.callPackage ./pkgs/eseries {
-      docopt-subcommands = pkgs.callPackage ./pkgs/docopt-subcommands { };
-    };
-    quart-schema = pkgs.callPackage ./pkgs/quart-schema { };
-    easyeda2ato = pkgs.callPackage ./pkgs/easyeda2ato { };
-  };
-  easyeda2ato = pkgs.callPackage ./pkgs/easyeda2ato { };
-  case-converter = pkgs.callPackage ./pkgs/case-converter { };
-  eseries = pkgs.callPackage ./pkgs/eseries {
-    docopt-subcommands = pkgs.callPackage ./pkgs/docopt-subcommands { };
-  };
-  docopt-subcommands = pkgs.callPackage ./pkgs/docopt-subcommands { };
-  quart-schema = pkgs.callPackage ./pkgs/quart-schema { };
-
-  easyeda2kicad = pkgs.callPackage ./pkgs/easyeda2kicad { };
-
-  # swingmusic = pkgs.callPackage ./pkgs/swingmusic { };
-
-  # ultimatevocalremovergui = pkgs.callPackage ./pkgs/ultimatevocalremovergui { };
-  # storyboarder = pkgs.callPackage ./pkgs/storyboarder { };
-  # paperlib = pkgs.callPackage ./pkgs/paperlib { };
-  # liteloaderqqnt = pkgs.callPackage ./pkgs/liteloaderqqnt { };
-  # icdea-stardnd = pkgs.callPackage ./pkgs/icdea-stardnd { };
-  # iceda-pro = pkgs.callPackage ./pkgs/iceda-pro { };
-  # rvemu = pkgs.callPackage ./pkgs/rvemu { };
-  # njuemu = pkgs.callPackage ./pkgs/njuemu { };
-  # gem5 = pkgs.callPackage ./pkgs/gem5 { };
-  # rife-ncnn-vulkan = pkgs.callPackage ./pkgs/rife-ncnn-vulkan { };
-  # real-esrgan = pkgs.callPackage ./pkgs/real-esrgan { };
-  # waifu2x-ncnn-vulkan = pkgs.callPackage ./pkgs/waifu2x-ncnn-vulkan { };
-  # terminalizer = pkgs.callPackage ./pkgs/terminalizer { };
-  # howdy = pkgs.callPackage ./pkgs/howdy { };
-  # o3de = pkgs.callPackage ./pkgs/o3de { };
 }
